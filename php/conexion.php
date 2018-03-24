a187fca5bf60fceb67727d2e72b3ef681abed4dd
Steven-Peraza <perazas376@gmail.com>
2018-03-23 20:25:31 -0600

    ACM1PT phpV4.4!

--- php/conexion.php
+++ php/conexion.php
@@ -1,21 +1,26 @@
 <?php
+// funcion que realiza la conexion con la base de datos
+// recibe de parámetros:
+//		el nombre de la DB a conectar
+//		el nombre de usuario y pass del mismo
+//		direccion IP de la conexion
+// 		el puerto donde se conecta
 
 function conexion($bd,$usuario,$pass,$ip,$puerto){
-	//phpinfo();
+	//se reciben los parámetros y se les asigna a las sig vars
     $serverName = "$ip"; 
 	$uid = "$usuario";   
 	$pwd = "$pass";  
 	$databaseName = "$bd"; 
 
+	//se hace un array con la información necesaria para realizar la conexión
 	$connectionInfo = array( "UID"=>$uid,                            
 							 "PWD"=>$pwd,                            
 							 "Database"=>$databaseName); 
 
-	/* Connect using SQL Server Authentication. */  
+	/* Se realiza la conexión usando una autentificación SQL Server */  
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);  
-
-//        or die('No se pudo conectar: ' . mysql_error());
-    
+	//en caso de que la conexión falle salta el mensaje de error...
 	if (!$conn) {
 		die('Algo fue mal mientras se conectaba a MSSQL');
 	}
